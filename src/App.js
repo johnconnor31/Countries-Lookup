0e14738fd221a21d484925b94c14ec1054ef6e1d
Sairam S <sairam.singireesu@reltio.com>
2021-09-25 19:34:06 +0530

    setup create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>
+        List of Countries
+      </h3>
+    </div>
+  );
+}
+
+export default App;
